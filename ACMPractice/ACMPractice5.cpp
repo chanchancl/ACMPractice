3a04529148c77f6a429eb6bccf2bfc5c49f57f0f
Ccl <540046768@qq.com>
2020-04-21 11:43:16 +0200

    water

--- ACMPractice/ACMPractice5.cpp
+++ ACMPractice/ACMPractice5.cpp
@@ -32,9 +32,37 @@ using namespace std;
 
 int main()
 {
-
+	int t;
+	cin >> t;
+	while(t--) {
+		int x, n, m;
+		cin >> x >> n >> m;
+		while(x > 0 && n && x / 2 + 10 < x) {
+			n--;
+			x = x / 2 + 10;
+		}
+		if (x <= 10 * m) 
+			cout << "YES" << endl;
+		else 
+			cout << "NO" << endl;
+	}
 	return 0;
 }
+
+
+/*
+int main()
+{
+	int n;
+	cin >> n;
+	while(n--) {
+		int a,b,c,d;
+		cin >> a >> b >> c >> d;
+		cout << b << " " << c  << " " << c << endl;
+	}
+	return 0;
+}
+*/
 
 /*
 int main()
